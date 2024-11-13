6d8c209552919fb58a03b49dc067b7e6f7a133ba
puedoiralbano <102501380+puedoiralbano@users.noreply.github.com>
2024-11-12 20:56:11 -0800

    open/create file and append

--- saving_to_log_file.py
+++ saving_to_log_file.py
@@ -1,17 +1,21 @@
 import os
-import datetime
-import pyzt
+from datetime import datetime
+import pytz
 
 def save_to_logfile(comment: str):
 
-    pst_timezone = pyzt.timezone("America/Los Angeles")
+    pst_timezone = pytz.timezone("America/Los_Angeles")
     current_date_pst = datetime.now(pst_timezone).date() #get current date
 
     curr_year = current_date_pst.year #get current year 
 
     filename = "logfile" + str(curr_year) + ".txt" #name of the logfile
 
-    with open(filename, 'a') as file:
+    print(filename)
+
+    with open(filename, 'a+') as file:
         file.write("Testing")
+
+        file.close()
 
     
