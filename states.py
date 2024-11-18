76c7162561432f373e31ce25a14a6f0668c9afbc
alan <alqliang@gmail.com>
2024-11-17 17:32:45 -0800

    Modified toGrid function to output a 2D array.

--- states.py
+++ states.py
@@ -12,8 +12,11 @@ class Grid:
         self.isHull = isHull
         self.isEmpty = isEmpty
         self.position = position
-        if isHull == False or isEmpty == False:
+        if isHull == False and isEmpty == False:
             self.setContainer(weight, name)
+        else:
+            self.weight = weight
+            self.name = name
     
     def setEmpty(self):
         self.isEmpty = True
@@ -27,7 +30,7 @@ class Grid:
         self.name = name    
 
 def toGrid(items: list["Item"]) -> np.array:
-    grid = np.empty(0)
+    grid = np.empty((8, 12), dtype=Grid)
     for item in items:
         # print(item['location'])
         # print(item['weight'])
@@ -39,12 +42,16 @@ def toGrid(items: list["Item"]) -> np.array:
                 isEmpty = True
             if item['company'] == "NAN":
                 isHull = True
-        temp = Grid(isHull, isEmpty, item['location'], item['weight'], item['company'])
-        grid = np.append(grid, temp)
+        pos = item['location']
+        temp = Grid(isHull, isEmpty, pos, item['weight'], item['company'])
+        x = pos[0]
+        y = pos[1]
+        grid[x - 1, y - 1] = temp
     return grid
 
 with open(f"SilverQueen.txt") as f:
     res = parse_manifest(f.read())
     arr = toGrid(res)
-    for square in arr:
-        print(square.name, square.weight)
+    for row in arr:
+        for square in row:
+            print(square.name, square.weight, square.isHull, square.isEmpty)
