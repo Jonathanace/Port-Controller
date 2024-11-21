81a4ab2d69d3dd80629bb49eae109be9e9e17593
Jonathanace <jonathanace@gmail.com>
2024-11-21 15:36:12 -0800

    Add tab grouping to separate Select Operation and Upload Manifest

--- frontend/app/func-select/page.tsx
+++ frontend/app/func-select/page.tsx
@@ -1,12 +1,13 @@
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-
+import { Separator } from "@/components/ui/separator"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export function ManifestUpload() {
   return (
     <div className="grid w-full max-w-sm items-center gap-1.5">
-      <Label htmlFor="Manifest">Upload Manifest</Label>
+      {/* <Label htmlFor="Manifest">Upload Manifest</Label> */}
       <Input id="manifest" type="file" />
     </div>
   )
@@ -21,19 +22,33 @@ export function FuncSelect() {
       </div>
       <div className="flex items-center space-x-2">
         <RadioGroupItem value="comfortable" id="r2" />
-        <Label htmlFor="r2">Load/ Unload</Label>
+        <Label htmlFor="r2">Load/Unload Operation</Label>
       </div>
     </RadioGroup>
   )
 }
 
-
 export default function Page() {
     return (
         <> 
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <FuncSelect />
-            <ManifestUpload />
+          <div>
+            
+          </div>
+            <Tabs defaultValue="select operation" className="w-[400px]">
+              <TabsList>
+                <TabsTrigger value="select operation">Select Operation</TabsTrigger>
+                <TabsTrigger value="upload manifest">Upload Manifest</TabsTrigger>
+              </TabsList>
+              <TabsContent value="select operation">
+                Select your desired operation.
+                <FuncSelect />
+              </TabsContent>
+              <TabsContent value="upload manifest">
+                Upload your manifest here.
+                <ManifestUpload />
+              </TabsContent>
+            </Tabs>
         </div>
         </>
     )
