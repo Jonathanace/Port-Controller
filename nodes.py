9f2afe1d64306c4d04319ed84c9847fa75a084a2
CharlesEOhagin <charles.ohagin@gmail.com>
2024-11-20 21:47:25 -0800

    fixed check aviable

--- nodes.py
+++ nodes.py
@@ -12,29 +12,36 @@ class Node:
     # checks if there is anything above the container, will be used in the move function
     # index [x,y]
     def check_above(self,index: tuple[int,int]) -> bool:
-        if self.ship[index[0]][index[1]].isEmpty == True:
+        if self.ship[index[0]][index[1]+1].isEmpty == True:
             return True
         return False
     # checks which possibe spot they can move to
     #index[x,y]
     def check_avaiable(self,index: tuple[int,int]):
         available_moves = []
-        for i in range(len(self.ship[0])):
+        for i in range(len(self.ship)):
             if i == index[0]:
                 continue
-            j = len(self.ship[0]) -1 
+            
+            j = len(self.ship[i]) -1 
             while j >= 0:
+                print(i,j)
+                
                 if self.ship[i][j].isEmpty == False:
-                    available_moves.append([i,j+1])
+                    print(self.ship[i][j].name)
+                    available_moves.append([i+1,j])
                     break
                 if j == 0:
                     available_moves.append([i,j])
-                    break
-                j+-1
+                j = j -1
+                
+            
         return available_moves
 
 with open(f"SilverQueen.txt") as f:
     res = parse_manifest(f.read())
 shipSilverQueen  = toGrid(res)
+print(len(shipSilverQueen))
+print(len(shipSilverQueen[0]))
 startNode = Node(shipSilverQueen)
-print(startNode.check_above([1,2]))
+print(startNode.check_avaiable([1,2]))
