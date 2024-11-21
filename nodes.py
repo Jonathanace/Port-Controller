db62dfb2f5edaea744b5c321fc8c18e7ac462e41
Alan Liang <44444316+alnliang@users.noreply.github.com>
2024-11-21 15:40:08 -0800

    Included edge cases in some node functions.

--- nodes.py
+++ nodes.py
@@ -12,6 +12,8 @@ class Node:
     # checks if there is anything above the container, will be used in the move function
     # index [x,y]
     def check_above(self,index: tuple[int,int]) -> bool:
+        if index[0] == 7:
+            return True
         if self.ship[index[0] + 1][index[1]].is_empty == True:
             return True
         return False
@@ -19,6 +21,8 @@ class Node:
     #index[x,y]
     def check_available(self,index: tuple[int,int]):
         available_moves = []
+        temp = self.ship[index[0], index[1]]
+        print(temp.position, temp.name)
         #columns
         for j in range(len(self.ship[0])):
             if j == index[1]:
@@ -29,13 +33,13 @@ class Node:
                 print(i, j, self.ship[i][j].position, self.ship[i][j].name)
                 if self.ship[i][j].is_empty == False:
                     print(self.ship[i][j].position, self.ship[i][j].name)
-                    available_moves.append([i+1,j])
+                    if i + 1 < 8:
+                        available_moves.append([i+1,j])
                     break
                 if i == 0:
                     print(self.ship[i][j].position, self.ship[i][j].name)
                     available_moves.append([i,j])
                     break
                 i = i -1
-                
-        print(available_moves)
+        # print(available_moves)
         return available_moves
