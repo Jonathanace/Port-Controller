01a70712fb50b169fae7d31feaf12106d6d069f5
CharlesEOhagin <charles.ohagin@gmail.com>
2024-11-21 14:11:29 -0800

    added movemnt type variable

--- step.py
+++ step.py
@@ -3,7 +3,8 @@ class Step:
     start_pos: tuple[int, int]
     end_pos: tuple[int, int]
     time_estimate: int
-    def __init__(self, start_pos, end_pos, time_estimate):
+    def __init__(self, start_pos, end_pos, time_estimate, movement_type):
         self.start_pos = start_pos
         self.end_pos = end_pos
         self.time_estimate = time_estimate
+        self.movement_type = movement_type
