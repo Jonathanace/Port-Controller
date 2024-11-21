8197c0c6c8ff6cbfba8d565dfb12c71216a78f13
alan <alqliang@gmail.com>
2024-11-21 13:48:15 -0800

    Implements movable_containers which lists our every container that can be moved at a given state.

--- balancing.py
+++ balancing.py
@@ -8,6 +8,7 @@ from utils import Item
 from utils import parse_manifest
 from states import Grid
 from states import to_grid
+from nodes import Node
 import numpy as np
 
 def can_balance(items: list["Item"]) -> bool:
@@ -31,7 +32,6 @@ def can_balance(items: list["Item"]) -> bool:
             return True
     return False
 
-def check_balanced()
 
 #Each step contains start pos and end pos
 def balance(items: list["Item"]):
@@ -40,11 +40,40 @@ def balance(items: list["Item"]):
         return(0)
     res = to_grid(items)
     start = Node(res)
-    frontier = [start.ship]
+    frontier = [start]
     explored = []
     steps = []
+    goal_reached = False
+    while goal_reached == False:
+        temp_frontier = []
+        for state in frontier:
+            movable = movable_containers(state)
+            for container in movable:
+                x = container.position[0] - 1
+                y = container.position[1] - 1
+                temp = tuple([x, y])
+                available = state.check_available(temp)
+                
+            
+    return(movable)
+
+def movable_containers(ship: Node):
+    curr = ship.ship
+    movable = []
+    for i in range(len(curr[0])):
+        j = len(curr) - 1
+        while j >= 0:
+            if curr[j][i].is_empty == False and curr[j][i].is_hull == False:
+                temp = tuple([j, i])
+                if ship.check_above(temp) == True:
+                    movable.append(temp)
+                    break
+            j -= 1
+    return movable
+
+def is_balanced(ship: Node):
+    curr = ship.ship
     
-    return(res)
 
 with open(f"SilverQueen.txt") as f:
     import time
@@ -52,8 +81,7 @@ with open(f"SilverQueen.txt") as f:
     res = parse_manifest(f.read())
     arr = balance(res)
     end_time = time.time()
-    for row in arr:
-        for square in row:
-            print(square.position, square.name, square.weight, square.isHull, square.isEmpty)
+    for square in arr:
+        print(square)
     time_spent = end_time - start_time
     print(time_spent, "seconds spent")
