94ac85d02e2df2379adaf2ededfe7996a72dea70
puedoiralbano <102501380+puedoiralbano@users.noreply.github.com>
2024-11-23 22:29:18 -0800

    added comments

--- save_state.py
+++ save_state.py
@@ -1,5 +1,7 @@
 import pickle
 
+# take the operation steps (balancing, loading/unloading) and the current step of operation and store into pickle file
+# example: displaying balancing step 5 and after moving onto step 6, save_state(solution_steps, 5) 
 def save_state(objects, curr_step: int): 
     try:
         with open("state.pkl", 'wb') as file:
@@ -7,7 +9,7 @@ def save_state(objects, curr_step: int):
     except:
         print("Could not save state into file")
 
-
+# check if a saved state file exists, and returns the saved state. If not prints an error.
 def get_state():
     try:
         with open('state.pkl', 'rb') as file:
