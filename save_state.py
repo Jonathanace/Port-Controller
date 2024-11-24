660fb57746c9ff4e8601420e31076a4b7067b603
puedoiralbano <102501380+puedoiralbano@users.noreply.github.com>
2024-11-23 23:04:13 -0800

    added manifest file name and type of operation to saving state

--- save_state.py
+++ save_state.py
@@ -1,15 +1,22 @@
 import pickle
+import string
 
-# take the operation steps (balancing, loading/unloading) and the current step of operation and store into pickle file
-# example: displaying balancing step 5 and after moving onto step 6, save_state(solution_steps, 5) 
-def save_state(objects, curr_step: int): 
+# take the type of operation, 
+# operation steps (balancing, loading/unloading), 
+# current step of operation, 
+# manifest file name 
+# and store into file
+# example: displaying balancing step 5 and after moving onto step 6, save_state("balancing", solution_steps, 5, "ShipCase1.txt") 
+def save_state(operation: string, objects, curr_step: int, manifest: string): 
     try:
         with open("state.pkl", 'wb') as file:
-            pickle.dump([objects, curr_step], file)
+            pickle.dump([objects, curr_step, manifest], file)
     except:
         print("Could not save state into file")
 
-# check if a saved state file exists, and returns the saved state. If not prints an error.
+# check if a saved state file exists
+#  and returns the saved state in a list containing type of operation, operation steps, current step of operation, and manifest file. 
+# If not prints an error.
 def get_state():
     try:
         with open('state.pkl', 'rb') as file:
