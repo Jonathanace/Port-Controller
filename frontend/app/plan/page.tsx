001275ca67bb7bb3745ca025b0ccb8d5c2a93e52
Jonathanace <jonathanace@gmail.com>
2024-11-25 16:26:55 -0800

    Add text entry box for operator comments

--- frontend/app/plan/page.tsx
+++ frontend/app/plan/page.tsx
@@ -10,6 +10,8 @@ import {
   CarouselPrevious,
   type CarouselApi,
 } from "@/components/ui/carousel"
+import { Textarea } from "@/components/ui/textarea"
+
 
 export function CarouselDApiDemo() {
   const [api, setApi] = React.useState<CarouselApi>()
@@ -57,6 +59,7 @@ export default function Page() {
     return <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
             <CarouselDApiDemo />
+            <Textarea />
         </main>
     </div>
 }
