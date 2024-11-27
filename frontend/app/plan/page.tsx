9e83d020e6dd468228f82d5fdfd83ba3c4aaa3ef
Jonathanace <jonathanace@gmail.com>
2024-11-26 19:12:49 -0800

    Add comment box placeholder text and Submit button

--- frontend/app/plan/page.tsx
+++ frontend/app/plan/page.tsx
@@ -13,6 +13,8 @@ import {
 import { Textarea } from "@/components/ui/textarea"
 import { HeaderProvider } from "@/context/HeaderContext";
 import Header from "@/components/ui/Header"
+import { Button } from "@/components/ui/button"
+
 
 
 export function CarouselDApiDemo() {
@@ -61,7 +63,11 @@ export default function Page() {
     return <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
             <CarouselDApiDemo />
-            <Textarea />
+            <div className="grid w-full gap-2">
+              <Textarea placeholder="Write your comments here."/>
+              <Button>Log Comment</Button>
+            </div>
+
         </main>
     </div>
 }
