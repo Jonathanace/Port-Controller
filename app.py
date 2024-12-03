0d8d22aea0a7403e00aba416d42e9e7809bc8640
Jonathanace <jonathanace@gmail.com>
2024-12-03 15:50:50 -0800

    Add routed process_manifest function to perform operations on uploaded manifest

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 from utils import parse_manifest
+from pprint import pprint
 
 app = Flask(__name__)
 CORS(app)
 UPLOAD_FOLDER = 'uploads'
+manifest_path = os.path.join(UPLOAD_FOLDER, 'manifest.txt')
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
@@ -19,14 +21,25 @@ def upload_file():
         return jsonify({'error': 'No selected file'}), 400
     if file:
 
-        file_path = os.path.join(UPLOAD_FOLDER, 'manifest.txt')
-        file.save(file_path)
-        # Pass the file path to the process_file function
+        file.save(manifest_path)
         
-        return '', 200
+        return '', 200 # FIXME: return ship's name here
 
-# @app.route('/process-manifest', methods=['POST'])
-# def process_manifest():
+@app.route('/process-manifest', methods=['POST'])
+def process_manifest():
+    data = request.get_json()
+    if not data['parse_option']: 
+        return jsonify({'error': 'No parsing option passed'}), 400
+    
+    with open(manifest_path) as file:
+        pprint(parse_manifest(file))
+    
+    if data['parse_option'] == 'balance':
+        pass
+    elif data['parse_option'] == 'load/unload':
+        pass # FIXME
+    else:
+        return jsonify({'error': 'Invalid parsing option passed'}), 400
 
 
 
