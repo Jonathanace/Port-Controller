ec5ceb7df05b460c30fe4bdb5f4a29cb1e17d0d4
Jonathanace <jonathanace@gmail.com>
2024-12-03 12:53:03 -0800

    Implement file uploading through flask

--- frontend/app/func-select/page.tsx
+++ frontend/app/func-select/page.tsx
@@ -23,7 +23,16 @@ interface ManifestUploadProps {
 }
 
 export function ManifestUpload({ onUpload } : ManifestUploadProps) {
-  const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const file = event.target.files[0]
+      const formData = new FormData();  
+      formData.append('file', file)
+      const response = await fetch('http://localhost:5000/upload', {
+        method: 'POST',
+        body: formData
+      });
+    }
     onUpload();
   };
 
