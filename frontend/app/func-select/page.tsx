929e84a4552d4553a790564110aee346f307bd19
Jonathanace <jonathanace@gmail.com>
2024-12-13 20:19:49 -0800

    Fix import error caused by fault rebase

--- frontend/app/func-select/page.tsx
+++ frontend/app/func-select/page.tsx
@@ -14,6 +14,7 @@ import { toast } from "@/hooks/use-toast";
 import { useHeader } from "@/context/HeaderContext";
 import { useRouter } from "next/navigation";
 import { Textarea } from "@/components/ui/textarea"
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog"
 
 
 
