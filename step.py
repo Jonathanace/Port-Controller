690a0a80e82a9a7f191649682b6575d4ed427170
CharlesEOhagin <charles.ohagin@gmail.com>
2024-12-13 18:14:29 -0800

    added name to step class, and weight to unload_load

--- step.py
+++ step.py
@@ -2,9 +2,10 @@ class Step:
     start_pos: tuple[int, int]
     end_pos: tuple[int, int]
     time_estimate: int
-    def __init__(self, start_pos, end_pos, time_estimate, movement_type, weight = None):
+    def __init__(self, start_pos, end_pos, time_estimate, movement_type, name = None,weight = None):
         self.start_pos = start_pos
         self.end_pos = end_pos
         self.time_estimate = time_estimate
         self.movement_type = movement_type
+        self.name = name
         self.weight = weight
